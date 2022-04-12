674c61eb1ea0565b2c9125413d835efe76f9c277
Aldo Rodriguez <aldo.rdgz@hotmail.com>
2022-04-12 13:12:17 -0500

    populate object to have data to randomize

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const deathObj = {
-    time : [],
-    monster : [],
-    how : []
+    time : ['three', 'two', 'fortyone', 'sixtysix', 'twenty', 'ten', 'twelve', 'five hundred', 'eight', 'eleven', 'ZERO'],
+    monster : ['ghoul', 'ghost', 'vampire', 'werewolf', 'demon', 'angel', 'djinn', 'bigfoot', 'alien', 'zombie', 'archangel'],
+    how : ['stabbing', 'eating', 'burning', 'shooting', 'dismembering', 'beheading', 'strangling', 'choking', 'drowning', 'defenestrating', 'hugging']
+    
 }
