458bcc0651522aacecbe327d7b7efa6b0b27bb35
Aldo Rodriguez <aldo.rdgz@hotmail.com>
2022-04-12 13:29:25 -0500

    integrated function to randomize

--- main.js
+++ main.js
@@ -2,5 +2,15 @@ const deathObj = {
     time : ['three', 'two', 'fortyone', 'sixtysix', 'twenty', 'ten', 'twelve', 'five hundred', 'eight', 'eleven', 'ZERO'],
     monster : ['ghoul', 'ghost', 'vampire', 'werewolf', 'demon', 'angel', 'djinn', 'bigfoot', 'alien', 'zombie', 'archangel'],
     how : ['stabbing', 'eating', 'burning', 'shooting', 'dismembering', 'beheading', 'strangling', 'choking', 'drowning', 'defenestrating', 'hugging']
-    
 }
+function catchElement(ele){
+    return ele[(Math.floor(Math.random() * ele.length))]
+}
+function mix(obj){
+    let deathTime = catchElement(obj.time);
+    let killer = catchElement(obj.monster);
+    let action = catchElement(obj.how);
+   
+    return`You will die in ${deathTime} days, a/an ${killer} will kill you by ${action} your sorry self.`;
+}
+console.log(mix(deathObj))
